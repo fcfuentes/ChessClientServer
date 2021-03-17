d72248a88af3e71573770b8cc21f07f41d6b2282
fcfuentes <80801865+fcfuentes@users.noreply.github.com>
2021-03-17 08:04:51 -0400

    Added comment to the file.

--- PlayerClient.h
+++ PlayerClient.h
@@ -1,3 +1,5 @@
+//Declaration of the client, PlayerClient for the server.
+
 #pragma once
 
 #include <string>
@@ -7,25 +9,42 @@ class PlayerClient
 {
 public:
     virtual ~PlayerClient() {}
+    
     PlayerClient();
+    
     PlayerClient(string nameValue);
+    
     PlayerClient(string nameValue, string countryValue, int eloRvalue);
+    
     PlayerClient(string nameValue, string countryValue);
+    
     string getPlayerName() { return playerName; };
+    
     string getCountry() { return country; };
+    
     int getEloRating() { return eloRating; };
+    
     int getID() { return playerID; };
+    
     void setPlayerName(string nameValue);
+    
     void setCountry(string countryValue);
+    
     void setEloRating(int eloRvalue);
+    
     std::string printData();
+    
     virtual void Notify(const std::string& msg) = 0;
 
 private:
     string playerName;
+    
     string country;
+    
     int eloRating;
+    
     int playerID;
+    
     static int ID;
 };
 
